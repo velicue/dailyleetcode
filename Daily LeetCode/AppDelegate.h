962f5286929669aa86aa01e90c16d127f84833dc
mandy <mandy@dhcp-ccc-2897.redrover.cornell.edu>
2016-03-09 20:23:58 -0500

    Initial Commit

---
+++ Daily LeetCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Daily LeetCode
+//
+//  Created by mandy on 16/3/9.
+//  Copyright © 2016年 mandy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Daily LeetCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Daily LeetCode
+//
+//  Created by mandy on 16/3/9.
+//  Copyright © 2016年 mandy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
